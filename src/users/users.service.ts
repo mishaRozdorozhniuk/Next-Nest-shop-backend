188093f2da23b0dc3af1efd6c46351a565726e31
Mykhailo Rozdorozhniuk <misha.roz.2003@gmail.com>
2025-06-10 17:47:11 +0200

    when create new user automaticaly set role as a USER

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,45 +1,3 @@
-// import { Injectable, UnprocessableEntityException } from '@nestjs/common';
-// import * as bcrypt from 'bcrypt';
-// import { CreateUserRequest } from './dto/create-user.request';
-// import { PrismaService } from 'src/prisma/prisma.service';
-// import { Prisma } from '@prisma/client';
-
-// @Injectable()
-// export class UsersService {
-//   constructor(private readonly prismaService: PrismaService) {}
-
-//   async createUser(data: CreateUserRequest) {
-//     const existingUser = await this.prismaService.user.findUnique({
-//       where: {
-//         email: data.email,
-//       },
-//     });
-
-//     // If user already exists, throw exception
-//     if (existingUser) {
-//       throw new UnprocessableEntityException('Email already exists');
-//     }
-
-//     // Otherwise create new user
-//     return this.prismaService.user.create({
-//       data: {
-//         ...data,
-//         password: await bcrypt.hash(data.password, 10),
-//       },
-//       select: {
-//         email: true,
-//         id: true,
-//       },
-//     });
-//   }
-
-//   async getUser(filter: Prisma.UserWhereUniqueInput) {
-//     return this.prismaService.user.findUniqueOrThrow({
-//       where: filter,
-//     });
-//   }
-// }
-
 import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { CreateUserRequest } from './dto/create-user.request';
@@ -56,6 +14,15 @@ export class UsersService {
         data: {
           ...data,
           password: await bcrypt.hash(data.password, 10),
+          roles: {
+            create: {
+              role: {
+                connect: {
+                  name: 'USER',
+                },
+              },
+            },
+          },
         },
         select: {
           email: true,
